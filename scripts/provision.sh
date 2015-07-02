1115ca534aee65b297b3b8f453a15ff041762f6c
Roger Ignazio <roger@puppetlabs.com>
2015-07-02 10:46:50 -0700

    remove trailing backslash

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,6 +3,6 @@
 puppet apply /vagrant/manifests/base.pp \
     --hiera_config /vagrant/hiera.yaml \
     --parser future \
-    --modulepath /vagrant/modules:/vagrant/vendor/modules \
+    --modulepath /vagrant/modules:/vagrant/vendor/modules
 
 exit 0
